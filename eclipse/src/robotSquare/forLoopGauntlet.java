27b5f5c8e60f0385c7e5dac1eef32b29b1a283d5
FinnVermaas <league@WTS21.attlocal.net>
2017-05-20 17:04:18 -0700

    made numbers!! magic!!

---
+++ eclipse/src/robotSquare/forLoopGauntlet.java
@@ -0,0 +1,16 @@
+package robotSquare;
+
+public class forLoopGauntlet {
+	public static void main(String[] args) {
+		
+	
+		
+	for(int i=2005;i>=2016; i++){
+		
+		System.out.println("In"+i+"I was"+(i-2005));
+		
+		
+	}
+	}
+
+}
