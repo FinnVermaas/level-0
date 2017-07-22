8928ccd640121f82d571260991e5ab019c8edc89
RhymeLyet <rhymeinparentheses@gmail.com>
2017-07-22 16:06:23 -0700

    for loop gauntlet

--- eclipse/src/robotSquare/forLoopGauntlet.java
+++ eclipse/src/robotSquare/forLoopGauntlet.java
@@ -3,14 +3,24 @@ package robotSquare;
 public class forLoopGauntlet {
 	public static void main(String[] args) {
 		
-	
+	int i=1;
 		
-	for(int i=2005;i>=2016; i++){
-		
-		System.out.println("In"+i+"I was"+(i-2005));
-		
-		
-	}
-	}
 
+	
+	for (int j2 = 1; j2 <= 100; j2=j2+10) {
+		
+for ( ; i < 11; i++) {
+	System.out.print(i);
+}
+	System.out.println();	
+	i=i+10;
+		
+		
+	//else{//
+	//System.out.println(i+"odd");	//
+	}
+	
+	
+	}
+	
 }
