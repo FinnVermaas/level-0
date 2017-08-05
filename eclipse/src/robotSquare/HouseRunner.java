d65c194f0af77c5dae98ea6bc3ab57fbc7b2d58a
varnit05 <league@WTS10.attlocal.net>
2017-08-05 16:02:05 -0700

    fkalsjdfhaewl

--- eclipse/src/robotSquare/HouseRunner.java
+++ eclipse/src/robotSquare/HouseRunner.java
@@ -6,7 +6,7 @@ public class HouseRunner {
 	public static void main(String[] args) {
 		Houses vancouver = new Houses();
 		vancouver.startInLeftCorner();
-		for(int i=0; i<10; i=i+1){
+		for(int i=0; i<7; i=i+1){
 			int random = new Random().nextInt(701);
 		vancouver.drawHouseWithGrass("large");
 		vancouver.drawHouseWithGrass("medium");
--- eclipse/src/robotSquare/Houses.java
+++ eclipse/src/robotSquare/Houses.java
@@ -10,7 +10,7 @@ public class Houses extends primeNumber {
 
 
 void startInLeftCorner(){
-	walle.moveTo(40, 770);
+	walle.moveTo(600, 770);
 	walle.move(1);
 	walle.setSpeed(10);
 	walle.setPenWidth(10);
@@ -20,9 +20,11 @@ void drawHouseWithGrass(int height){
  walle.penDown();
 
 	walle.move(height);	
-walle.turn(90);
+walle.turn(30);
 walle.move(40);
 walle.turn(90);
+walle.move(40);
+walle.turn(30);
 walle.move(height);
 walle.turn(-90);
 walle.setPenColor(Color.green);
@@ -33,12 +35,15 @@ walle.setPenColor(Color.black);
 void drawHouseWithGrass(String size){
 	if(size.equals("large")){
 		drawHouseWithGrass(250);
+		walle.setPenColor(255, 0, 0);
 	}
 	if(size.equals("medium")){
 		drawHouseWithGrass(120);
+		walle.setPenColor(0, 0, 255);
 }
 	if(size.equals("small")){
 		drawHouseWithGrass(60);
+		walle.setPenColor(100, 0, 100);
 }
 }
 }
